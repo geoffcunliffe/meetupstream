2ffad532132fdb7d9b49278e51067d2a022ed3b6
Geoff <geoffcunliffe@gmail.com>
2016-03-30 10:40:41 +1100

    Simple Parsed Json Stream of Meetups RSVPs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-
 /**
  * Created by geoff on 25/03/2016.
  */
@@ -16,18 +15,20 @@ public class App {
     public static void main(String[] args) {
 
         JSONObject obj = null;
-        URL url = null;
         InputStream in = null;
+
         try {
-            url = new URL("http://stream.meetup.com/2/rsvps");
+            //Make new connection to the http stream
+            URL url = new URL("http://stream.meetup.com/2/rsvps");
             HttpURLConnection httpcon = (HttpURLConnection) url.openConnection();
             httpcon.addRequestProperty("User-Agent", "Geoff's Test Java App");
-            in = httpcon.getInputStream();
 
+            in = httpcon.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
             String line;
 
+            //Read a line from the http stream and parse as Json object
             while ((line = reader.readLine()) != null) {
                 obj = new JSONObject(line);
                 String venueName = "Null";
